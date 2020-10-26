cf64353ee967a908dc34cbafb74f3be955426b65
TurniXXD <legokubik@seznam.cz>
2020-10-26 15:16:37 +0100

    connection

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+require('dotenv').config({path:__dirname+'/.env'});
 
 //import routes
 const authRoute = require('./routes/auth');
-const postRoute = require('./routes/posts')
-
-dotenv.config();
-
-//mongoose deprecation warnings fix
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useNewUrlParser', true);
+const postRoute = require('./routes/posts');
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+const uri = process.env.DB_CONNECT;
+const db = mongoose.connection;
 
 //Connect to DB
 //mongodb+srv://<username>:<password>@ultimatecluster.wb4ba.mongodb.net/<dbname>?retryWrites=true&w=majority
 //<username> = admin, <password> = lz7QqsmQCdL52BU2, <dbname> = test
-mongoose.connect(
-	process.env.DB_CONNECT, 
-	() => console.log('connect to DB')
-);
+mongoose.connect(uri, options);
+
+db.on('error', (err)=>{
+	console.log('>> Failed to connect to MongoDB, retrying...');
+
+	setTimeout( () => {
+		mongoose.connect(uri, options);
+		},5000);
+});
+
+db.once('open',() => {
+	console.log("connected to DB");
+});
 
 //Middleware
 app.use(express.json());
